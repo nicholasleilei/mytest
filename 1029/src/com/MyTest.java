288df2ae235cc5392045b6bbaf8acec8a3a903e4
nicholasleilei <349417084@qq.com>
2019-10-29 17:51:54 +0800

    okle

--- 1029/src/com/MyTest.java
+++ 1029/src/com/MyTest.java
@@ -4,7 +4,7 @@ public class MyTest {
     public void name() {
 		System.out.println("11111okokok");
 		/*
-		 * 把本地工程提交到远程  commitand push 推
+		 * 把本地工程提交到远程  commitand   push 推
 		 * 将远程仓库的工程 下载到本地    pull  拉
 		 */
 	}
