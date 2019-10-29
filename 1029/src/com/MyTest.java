3a79775a4ba572fb1da9602e97f67c085bbf4d4f
nicholasleilei <349417084@qq.com>
2019-10-29 17:40:32 +0800

    用户2修改完成10-29

--- 1029/src/com/MyTest.java
+++ 1029/src/com/MyTest.java
@@ -2,6 +2,10 @@ package com;
 
 public class MyTest {
     public void name() {
-		System.out.println("11111");
+		System.out.println("11111okokok");
+		/*
+		 * 把本地工程提交到远程  commitand push 推
+		 * 将远程仓库的工程 下载到本地    pull  拉
+		 */
 	}
 }
