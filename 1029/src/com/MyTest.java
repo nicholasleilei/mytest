e75bf223588f334314ef867d0acaa9d36a049d25
nicholasleilei <349417084@qq.com>
2019-10-29 17:47:27 +0800

    ok

--- 1029/src/com/MyTest.java
+++ 1029/src/com/MyTest.java
@@ -3,8 +3,11 @@ package com;
 public class MyTest {
     public void name() {
 
-		System.out.println("11111wanchengokyonghu1");
+		System.out.println("11111我自己的空间我说的算ok");
 //我对
-
+/*
+ * 出现了冲突
+ * 解决办法：先pull 再push
+ */
 	}
 }
